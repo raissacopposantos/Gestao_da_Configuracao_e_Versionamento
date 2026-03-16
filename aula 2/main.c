9f2a548265025d7d349a232d962fbfa6851bf87b
Raíssa Coppo Santos <raissa.coppo.santos@gmail.com>
2026-03-16 08:43:14 -0300

    Add files via upload

---
+++ aula 2/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Atualização Professor!\n")
+    printf("Hello, World!\n");
+    printf("Aluno editando. v2\n");
+    printf("Aluno editando. v3\n");
+    return 0;
+}
