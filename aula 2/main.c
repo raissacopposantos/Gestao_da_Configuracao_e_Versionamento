3d31b8ccd6993eb6b09780ae62c74e07c840ac3f
Raíssa Coppo Santos <raissa.coppo.santos@gmail.com>
2026-04-05 14:35:26 -0300

    Delete aula 2 directory

--- aula 2/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Atualização Professor!\n")
-    printf("Hello, World!\n");
-    printf("Aluno editando. v2\n");
-    printf("Aluno editando. v3\n");
-    return 0;
-}
